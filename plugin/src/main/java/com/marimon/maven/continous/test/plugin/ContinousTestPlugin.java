442d0cba2a59f8e94ca3a97260a748a5603be3cc
Ignasi Marimon-Clos <ignasi35@gmail.com>
2012-07-03 21:26:38 +0200

    Implement the file modification detection.

--- plugin/src/main/java/com/marimon/maven/continous/test/plugin/ContinousTestPlugin.java
+++ plugin/src/main/java/com/marimon/maven/continous/test/plugin/ContinousTestPlugin.java
@@ -1,6 +1,13 @@
 package com.marimon.maven.continous.test.plugin;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel.MapMode;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +28,7 @@ import org.apache.maven.plugin.surefire.SurefireExecutionParameters;
 import org.apache.maven.plugin.surefire.SurefirePlugin;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.toolchain.ToolchainManager;
+import org.codehaus.plexus.util.Base64;
 
 /**
  * Maven plugin to continously run tests. Developed following the guide at:
@@ -33,7 +41,11 @@ import org.apache.maven.toolchain.ToolchainManager;
 public class ContinousTestPlugin extends AbstractMojo implements
         SurefireExecutionParameters {
 
+    public static final long DELAY = 200;
+
     private final SurefirePlugin surefirePlugin;
+
+    private String _previousHash = "";
 
     public ContinousTestPlugin() {
         this(new SurefirePlugin());
@@ -53,17 +65,87 @@ public class ContinousTestPlugin extends AbstractMojo implements
         while (runs < 10) {
             getLog().info("Checking files...");
 
-            @SuppressWarnings("rawtypes")
-            ConcurrentHashMap ctx = new ConcurrentHashMap();
-            ctx.putAll(getPluginContext());
-            surefirePlugin.setPluginContext(ctx);
+            while (!detectedChange()) {
+                try {
+                    Thread.sleep(DELAY);
+                } catch (InterruptedException e) {
+                }
+            }
+            launch();
 
-            surefirePlugin.execute();
-            // updateTarget();
             getLog().info("Files checked. " + runs);
             runs++;
         }
         getLog().info("Continous Test Plugin completed.");
+    }
+
+    private boolean detectedChange() {
+        String currentHash =
+            new String(Base64.encodeBase64(getCurrentHash()));
+        boolean equals = _previousHash.equals(currentHash);
+        if (!equals) {
+            _previousHash = currentHash;
+        }
+        return !equals;
+    }
+
+    private byte[] getCurrentHash() {
+        MessageDigest instance;
+        try {
+            instance = MessageDigest.getInstance("MD5");
+            updateHash(getBasedir(), instance);
+            updateHash(getTestSourceDirectory(), instance);
+            return instance.digest();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void updateHash(final File file, final MessageDigest instance) {
+        if (file != null) {
+            instance.update(file.getName().getBytes());
+            if (file.isDirectory()) {
+                File[] listFiles = file.listFiles();
+                for (File f : listFiles) {
+                    updateHash(f, instance);
+                }
+            } else {
+                updateHashFile(instance, file);
+            }
+        } else {
+            instance.update((byte) System.currentTimeMillis());
+        }
+
+    }
+
+    private void updateHashFile(final MessageDigest instance, final File f) {
+        FileInputStream input = null;
+        ByteBuffer bb;
+        try {
+            input = new FileInputStream(f);
+            try {
+                bb =
+                    input.getChannel().map(MapMode.READ_ONLY, 0,
+                        f.length());
+                instance.update(bb);
+            } finally {
+                if (input != null) {
+                    input.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+
+    }
+
+    private void launch()
+            throws MojoExecutionException, MojoFailureException {
+        @SuppressWarnings("rawtypes")
+        ConcurrentHashMap ctx = new ConcurrentHashMap();
+        ctx.putAll(getPluginContext());
+        surefirePlugin.setPluginContext(ctx);
+        surefirePlugin.execute();
     }
 
     @SuppressWarnings("rawtypes")
--- plugin/src/test/java/com/marimon/maven/continous/test/plugin/ContinousTestPluginTest.java
+++ plugin/src/test/java/com/marimon/maven/continous/test/plugin/ContinousTestPluginTest.java
@@ -1,15 +1,20 @@
 package com.marimon.maven.continous.test.plugin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import junit.framework.Assert;
 
 import org.apache.maven.monitor.logging.DefaultLog;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.surefire.SurefirePlugin;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -17,27 +22,47 @@ import org.junit.Test;
  */
 public class ContinousTestPluginTest {
 
-    @Test
-    public void testContinousTestPluginImplementsMojoInterface()
-            throws Exception {
-        try {
-            Mojo mojo = new ContinousTestPlugin();
-            mojo.getLog();
-        } catch (ClassCastException e) {
-            Assert.fail("The cast should be valid.");
-        }
-    }
+    private ContinousTestPlugin _mojo;
+
+    private File _tmpFolder;
 
     @SuppressWarnings("rawtypes")
+    @Before
+    public void before() {
+        _mojo = new ContinousTestPlugin(new FakeSurefirePlugin());
+        _mojo.setPluginContext(new ConcurrentHashMap());
+
+        _tmpFolder =
+            new File(System.getProperty("java.io.tmpdir"),
+                ContinousTestPluginTest.class.getName()
+                    + Long.toString(System.currentTimeMillis()));
+        _tmpFolder.mkdirs();
+    }
+
+    @After
+    public void after() {
+        delete(_tmpFolder);
+    }
+
+    private void delete(final File file) {
+        if (file.isDirectory()) {
+            File[] listFiles = file.listFiles();
+            for (File f : listFiles) {
+                if (f.isDirectory()) {
+                    delete(f);
+                }
+                f.delete();
+            }
+        }
+        file.delete();
+    }
+
     @Test
     public void testLoggerOutputsDataRegardingThePluginWhenExecuted()
             throws Exception {
-        AbstractMojo mojo =
-            new ContinousTestPlugin(new FakeSurefirePlugin());
-        mojo.setPluginContext(new ConcurrentHashMap());
         FakeLog log = new FakeLog();
-        mojo.setLog(log);
-        mojo.execute();
+        _mojo.setLog(log);
+        _mojo.execute();
         Assert.assertEquals(
             "Continous Test Plugin started...Checking files..."
                 + "Files checked. 0Checking files...Files checked. 1"
@@ -51,6 +76,56 @@ public class ContinousTestPluginTest {
                 + "Checking files...Files checked. 9"
                 + "Continous Test Plugin completed.", log.getSb()
                 .toString());
+    }
+
+    @Test
+    public void testWaitsUntilAChangeIsDetectedInSourceFolders()
+            throws Exception {
+
+        FakeLog log = new FakeLog();
+        _mojo.setLog(log);
+
+        _mojo.setTestSourceDirectory(mkdir("test"));
+        _mojo.setBasedir(mkdir("baseDir"));
+
+        touch(_mojo.getTestSourceDirectory(), "SampleTest.java");
+        touch(_mojo.getBasedir(), "Sample.java");
+
+        ExecutorService pool = Executors.newFixedThreadPool(1);
+        pool.submit(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    _mojo.execute();
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        Thread.sleep(ContinousTestPlugin.DELAY * 2);
+        touch(_mojo.getBasedir(), "Some.java");
+        Thread.sleep(ContinousTestPlugin.DELAY * 5);
+        Assert
+            .assertEquals(
+                "Continous Test Plugin started...Checking files..."
+                    + "Files checked. 0Checking files...Files checked. 1Checking files...",
+                log.getSb().toString());
+        pool.shutdownNow();
+    }
+
+    private void touch(final File basedir, final String name)
+            throws IOException {
+        File file = new File(basedir, name);
+        FileOutputStream out = new FileOutputStream(file);
+        out.write(Long.toString(System.currentTimeMillis()).getBytes());
+        out.close();
+    }
+
+    private File mkdir(final String dirname) {
+        File mkdir = new File(_tmpFolder, dirname);
+        mkdir.mkdirs();
+        return mkdir;
     }
 
     private class FakeSurefirePlugin extends SurefirePlugin {
